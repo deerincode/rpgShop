d5f75b1befe7e9f666791ab02d793bc500991965
Vogue Doesong <nboney@email.sc.edu>
2022-12-21 22:50:35 -0500

    product page items displaying in desired format

--- views/Index.jsx
+++ views/Index.jsx
@@ -7,27 +7,27 @@ class Index extends React.Component {
 
         return(
 
-            <div>
+            <div className='page'>
                 <link rel="stylesheet" type="text/css" href="../css/index.css" />
                 <h1>Welcome to the JRPG Costco!!</h1>
+                {/* <div className='container'> */}
+                    <ul>
+                        {this.props.item.map((item, i) => {
+                            return (
+                                // <li>
+                                //     <a href={`/products/${item.id}`}>{item.name}
+                                //     </a>
+                                // </li>
 
-                <ul>
-                    {this.props.item.map((item, i) => {
-                        return (
-                            // <li>
-                            //     <a href={`/products/${item.id}`}>{item.name}
-                            //     </a>
-                            // </li>
-                            
-                            <div className='itemBox'>
-                                <h4>{item.name}</h4>
-                                <img src={`${item.img}`} alt="" />
-                                <h2>${item.price}</h2>
-                            </div>
-                        )
-                    })}
-                </ul>
-
+                                <div className='itemBox'>
+                                    <a href={`/products/${item.id}`}><h4>{item.name}</h4></a>
+                                    <img src={`${item.img}`} alt="" />
+                                    <h2>${item.price}</h2>
+                                </div>
+                            )
+                        })}
+                    </ul>
+                {/* </div> */}
 
 
 
